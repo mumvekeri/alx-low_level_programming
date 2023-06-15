98ed782c2a912a11769be8b73f6912aef5b604a1
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-15 10:57:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 */
 int main(void)
 {
-const char[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, strlen(message));
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, strlen(message));
 return (1);
 }
 
