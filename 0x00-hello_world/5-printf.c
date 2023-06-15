1bd7acb8d41b3cb4e0f579fcbbd51d8c01c7179b
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-15 07:49:31 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Returm: Always 0 (Success)
- */
+* main - Entry point
+*
+* Returm: Always 0 (Success)
+*/
 int main(void) 
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
