c4d863d20cf4b65f2830fc91dc4dd0f1f619dd33
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-15 10:13:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 /**
-* main - A c program that prints a line to the standard error
+* main - A program that prints a line to the standard error
 * Return: 1 (Success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, strlen(message));
 return (1);
 }
+
