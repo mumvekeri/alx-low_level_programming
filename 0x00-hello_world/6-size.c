5936595e9485f329cde80de058c1c57e7bf8960e
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-15 16:42:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Description: Uses the puts function to print a message
-*
-* Return: Always 0 (Success)
+* main - Lines which print the size of several types.
+* Return: 0 (Success)
 */
 #include <stdio.h>
 int main(void)
 {
-printf("Size of char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
 
