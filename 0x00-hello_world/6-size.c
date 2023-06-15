600450cacfd49c3264f992ef002303ff5494330a
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-15 09:18:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-printf( printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of char: %ld byte(s)\n", sizeof(char));
 printf("Size of int: %ld byte(s)\n", sizeof(int));
 printf("Size of long: %ld byte(s)\n", sizeof(long));
 printf("Size of long long: %ld byte(s)\n", sizeof(long long));
