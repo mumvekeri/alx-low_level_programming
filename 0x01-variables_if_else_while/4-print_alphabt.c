b972acf7c975e59585601a62f7f7f35b5f65edfa
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 10:34:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,9 @@ int main(void)
 {
 char cha;
 for (cha = 'a'; cha <= 'z'; cha++)
-
-{	
-if (cha != 'q' && cha != 'e')
-putchar(cha);
+{
+	if (cha != 'q' && cha != 'e')
+		putchar(cha);
 }
 putchar('\n');
 return (0);
