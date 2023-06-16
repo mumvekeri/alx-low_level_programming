95862e73bc6cd042bbb3b94dcc52f05c03683782
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 16:35:13 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of two digits.
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+int x;
+for (n = 0; n <= 9; n++)
+{
+for (x = 0; x <= 9; x++)
+{
+if (n >= x)
+continue;
+putchar(n + '0');
+putchar(x + '0');
+if ((n != 8) || (x != 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
