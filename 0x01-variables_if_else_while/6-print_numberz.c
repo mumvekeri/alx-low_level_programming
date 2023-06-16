89406b1f4c2b064abf77c4527fda9784649f1fd6
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 14:45:46 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,11 @@
 * main - Entry point
 * Return: 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 int num;
 for (num = 0; num <= 9; num++)
-putchar((num % 9) + 0);
-putchar('\n');
+putchar((num % 9), +'0')
+putchar("\n");
 return (0);
 }
-
