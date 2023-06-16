58f65844ebefa87001d638be0ba0c1c45c2da1c0
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 15:14:01 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 char cha;
 int num;
 for (num = 0; num < 10; num++)
-putchar((num % 10) + 0);
+putchar((num % 10) + '0');
 for (cha = 'a'; cha <= 'f'; cha++)
 putchar(cha);
 putchar('\n');
