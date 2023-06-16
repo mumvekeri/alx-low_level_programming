b887c7c5024cbbbf0fffa4a01daaac23b7972c03
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 06:47:55 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,8 @@ if (a > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, a);
 }
-else if (a == 0) {
+else if (a == 0)
+{
 printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
