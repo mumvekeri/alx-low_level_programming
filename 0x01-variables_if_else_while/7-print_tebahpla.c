1b573e0d77d47e47eb550d0d7fa617c363d51d92
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 14:09:16 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char cha;
+for (cha = 'z'; cha >= 'a'; cha--)
+putchar(cha);
+putchar('\n');
+return (0);
+}
+
