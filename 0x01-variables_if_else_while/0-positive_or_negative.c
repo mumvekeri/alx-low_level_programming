5c25c78536219105f0f060ef99fe9d8b7857cf39
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-16 05:41:19 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive\n");
+printf("is positive\n");
 }
 else if (n == 0)
 {
-printf("%d is zero\n");
+printf("is zero\n");
 }
 else
 {
-printf("%d is negative\n");
+printf("is negative\n");
 }
 
 return (0);
