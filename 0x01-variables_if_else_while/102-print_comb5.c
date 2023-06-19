90c44cda35a1f6b4c995a3eb555958afcaf9d9a7
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-19 00:40:09 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,38 +8,25 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 99; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (a > b)
-				continue;
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+			putchar('0' + b / 10);
+			putchar('0' + b % 10);
 
-			if (!(a == 8 && b == 9))
-				putchar(' ');
-		}
-	for (c = 0; c <= 9; c++)
-	{
-		for (d = 0; d <= 9; d++)
-		{
-			if (c > d)
-				continue;
-			putchar((c % 10) + '0');
-			putchar((d % 10) + '0');
-
-			if (!(c == 8 && d == 9))
-			{
+			if (a == 98 && b == 99)
+				break;
+			putchar(' ');
 			putchar(',');
 			putchar(' ');
-			}
 		}
 	}
-	}
+
+
 	putchar('\n');
 	return (0);
 }
