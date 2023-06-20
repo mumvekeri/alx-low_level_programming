8c3720ff153fee49859e3108303bf8b73a3e6d7e
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-20 22:31:00 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main - a program that prints _putchar
- * Return: 0 (Success)
- */
+* main - a program that prints _putchar
+* Return: 0 (Success)
+*/
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
