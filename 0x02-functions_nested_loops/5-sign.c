1fd42a93484117a3f8b06373f30c6f53344cbacb
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-21 12:14:52 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		putchar(48);
-		return (0);
+		putchar('-');
+		return (-1);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
-		return (-1);
+		putchar('0');
+		return (0);
 	}
 }
