315d4a069d08b9448851ed7059919f704f037af5
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-21 12:38:08 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,30 +8,22 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a < 2; a++)
+	a = 0;
+
+	while (a < 24)
 	{
-		for (b = 0; b <= 9; b++)
+		b = 0;
+		while (b < 60)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a == 2 && b > 3)
-					{
-						break;
-					}
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(':');
-					putchar(c + '0');
-					putchar(d + '0');
-					putchar('\n');
-				}
-			}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(':');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar('\n');
+			b++;
 		}
+		a++;
 	}
 }
-
