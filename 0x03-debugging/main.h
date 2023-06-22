e3700a469fc38b39180349e5600724d672c97c4f
mumvekeri <mumvekerichidochashe@gmail.com>
2023-06-22 12:59:32 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,5 @@
-#ifndef main_h
-#define main_h
-_putchar(char);
-void positive_or_negative(i);
+#ifndef MAIN_H
+#define MAIN_H
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
