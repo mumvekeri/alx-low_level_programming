20df50c3f6c2134bde505a98f0c4c42beefba98f
mumvekeri <mumvekerichidochashe@gmail.com>
2023-07-17 15:38:28 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - a program that prints the name of the file
- * Return: 0 (Success)
+ *
+ * Result: 0 (Success)
  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", _FILE_);
+	printf("%s\n", _FILE_);
 	return (0);
 }
