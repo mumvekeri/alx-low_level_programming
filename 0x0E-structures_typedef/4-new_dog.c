c07ccb25c934d7ef8458b82478b91b2c82f30224
mumvekeri <mumvekerichidochashe@gmail.com>
2023-07-17 14:28:49 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcpy - a program that copies the string pointed to by src
+ * *_strcpy - a program that copy the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
