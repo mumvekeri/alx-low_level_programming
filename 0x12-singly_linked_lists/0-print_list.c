0bb58bbd3e593f82275c3a71bec4212874465dbe
mumvekeri <mumvekerichidochashe@gmail.com>
2023-07-27 23:29:02 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str != NULL ? h->str : "(nil)\n");
+		printf("[%u] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		cat++;
 	}
