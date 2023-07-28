fa9ba4707d610c9d92f3a9dad6541ce06df56829
mumvekeri <mumvekerichidochashe@gmail.com>
2023-07-28 00:32:22 +0000

    0-print_list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *  * list_len - returns the number of elements in a list
+ *   * @h: the list_t list
+ * Return: 0 (Success)
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (j)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
