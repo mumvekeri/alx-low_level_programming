34a09fcc594eb7b8f65063753c2fd3643c1b8fd2
Michelle Chiza <Chiza.tadiwanashe@gmail.com>
2023-07-31 08:51:36 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: linked list of listint_t to print
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int x;
+
+	while (h)
+	{
+		printf("%i\n", h->n);
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: linked list of listint_t to print
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	int x;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
