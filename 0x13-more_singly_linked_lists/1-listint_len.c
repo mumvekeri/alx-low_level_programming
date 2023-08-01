31aa85bb2c978fdd2546978ee0e4e8597602d5be
mumvekeri <mumvekerichidochashe@gmail.com>
2023-08-01 11:33:27 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: linked list of listint_t to print
+ * Return: number of elements in a linked
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
