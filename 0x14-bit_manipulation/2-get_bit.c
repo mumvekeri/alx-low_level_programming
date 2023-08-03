ed900f206636426161ae966ac5397f8f90be8e21
mumvekeri <mumvekerichidochashe@gmail.com>
2023-08-03 03:39:19 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,6 +2,7 @@
 /**
  * get_bit - function that returns the value of a bit at a given index
  * @index: is the index, starting from 0 of the bit you want to get.
+ * @n: binary number
  * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
