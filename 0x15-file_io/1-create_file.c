4abe0d7e596a508613739d678214795a0dfdd7e6
mumvekeri <mumvekerichidochashe@gmail.com>
2023-08-07 03:45:15 -0700

    0-read_textfile.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,3 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int a;
+	int b;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (a = 0; text_content[a];)
+			a++;
+	}
+	 int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	 b = write(fd, text_content, a);
+
+	 if (fd == -1 || b == -1)
+		 return (-1);
+	 close(fd);
+
+	 return (1);
+
+}
