0d1dfede8aa895fbd841a1908de529ecab9804ea
mumvekeri <mumvekerichidochashe@gmail.com>
2023-08-07 04:18:19 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,11 +8,12 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-		return (-1);
 	int fd;
 	int a;
 	int c = 0;
+
+	if (filename == NULL)
+		return (-1);
 
 	if (text_content != NULL)
 	{
