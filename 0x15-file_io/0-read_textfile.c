91dbf11beeb859c0fdb1f3c22a26161971196129
mumvekeri <mumvekerichidochashe@gmail.com>
2023-08-07 03:29:24 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * read_textfile -  function that reads a text file and prints it to the POSIX
+ * @letters: is the number of letters it should read and print.
+ * @filename: name assigned to a file.
+ * Return:  actual number of letters it could read and print. Reurn 0 if,
+ * the file can not be opened or read, if filename is NULL, or if write fails
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i;
+	ssize_t result;
+	ssize_t fd;
+	char *buffer;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	i = read(fd, buffer, letters);
+	result = write(STDOUT_FILENO, buffer, i);
+
+	free(buffer);
+	close(fd);
+	return (result);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name assigned to the file
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
