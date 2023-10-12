a730a508e6af76bf7eb30cd216627c79bdb36353
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-12 13:28:52 -0700

    crackme

--- 0x17-doubly_linked_lists/crackme4.py
+++ 0x17-doubly_linked_lists/crackme4.py
@@ -1,10 +1,4 @@
-# uncompyle6 version 3.9.0
-# Python bytecode version base 3.4 (3310)
-# Decompiled from: Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
-# [GCC 9.4.0]
-# Embedded file name: 100-crackme.py
-# Compiled at: 2017-01-05 16:44:37
-# Size of source mod 2**32: 195 bytes
+#!/usr/bin/python3
 pwd = input('Password: ')
 ok = 'Zen of Python'
 ok = ok + ' C'
@@ -13,4 +7,3 @@ if pwd == ok:
     print('OK')
 else:
     print('KO')
-# okay decompiling crackme4.pyc
